7b325d60bb431fae7a3974285c48b2581711065f
Rakibul Hassan <abrouf664@gmail.com>
2026-06-28 20:27:44 +0600

    Add a Delete Event Api

--- index.js
+++ index.js
@@ -79,6 +79,16 @@ async function run() {
   res.send(result);
 });
 
+app.delete("/events/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const result = await eventsCollection.deleteOne({
+    _id: new ObjectId(id),
+  });
+
+  res.send(result);
+});
+
 
 
 
